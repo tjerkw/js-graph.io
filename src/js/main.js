5da9f13739e7dd2ad8543aca5cb8c5bf7cac1ee7
tjerkw <t.w.=d1@h>
2013-01-23 14:25:57 +0100

    Added some modals, added serve script

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,29 @@
 $(function() {
-	
+
 	Graph.init();
 	Editor.init();
+
+	console.log("Showing welcome");
+	doOnce(showWelcome);
 });
+
+/**
+ * Perform the function f only once on a computer.
+ * It remembers the state between page refreshes using cookies.
+ *
+ * @param f the function to execute once, doesnt work with anonymous functions
+ */
+function doOnce(f) {
+
+	if($.cookie(f.name) === null) {
+		$.cookie(f.name, "true");
+		f();
+	}
+}
+
+function showWelcome() {
+	window.setTimeout(function() {
+
+		$("#welcomeModal").modal({});
+	}, 500);
+}
